68fd367e055cfbfd59ec6496801264d146219bd3
HealKnix <www.daniiland@gmail.com>
2023-11-08 20:43:48 +0800

    Проверка музыки на деплое

--- main.js
+++ main.js
@@ -2,37 +2,38 @@ import * as Plants from '/src/models/Plants'
 import * as Zombie from '/src/models/Zombies'
 
 document.addEventListener('DOMContentLoaded', function () {
-	var audio = document.getElementById('music')
+  var audio = document.getElementById('music')
+  audio.volume = 0.25
 
-	audio.addEventListener('ended', function () {
-		audio.play()
-	})
+  audio.addEventListener('ended', () => {
+    audio.play()
+  })
 })
 
 let suns = 0
 let deltaTime = 0
 
 const map = [
-	{
-		plantsArray: new Array(),
-		zombiesArray: new Array(),
-	},
-	{
-		plantsArray: new Array(),
-		zombiesArray: new Array(),
-	},
-	{
-		plantsArray: new Array(),
-		zombiesArray: new Array(),
-	},
-	{
-		plantsArray: new Array(),
-		zombiesArray: new Array(),
-	},
-	{
-		plantsArray: new Array(),
-		zombiesArray: new Array(),
-	},
+  {
+    plantsArray: new Array(),
+    zombiesArray: new Array()
+  },
+  {
+    plantsArray: new Array(),
+    zombiesArray: new Array()
+  },
+  {
+    plantsArray: new Array(),
+    zombiesArray: new Array()
+  },
+  {
+    plantsArray: new Array(),
+    zombiesArray: new Array()
+  },
+  {
+    plantsArray: new Array(),
+    zombiesArray: new Array()
+  }
 ]
 
 let floor_row = document.querySelectorAll('.floor__row')
@@ -62,39 +63,38 @@ floor_row.forEach(row => {
 })
 
 requestAnimationFrame(function selectedCeil() {
-	map.forEach(lane => {
-		lane.plantsArray.forEach(plant => {
-			if (
-				lane.zombiesArray.length > 0 &&
-				lane.zombiesArray.some(
-					zombie =>
-						zombie.htmlElement.getBoundingClientRect().x +
-							zombie.htmlElement.getBoundingClientRect().width / 3 >
-						plant.htmlElement.getBoundingClientRect().x
-				)
-			) {
-				plant.shoot()
-			}
-			plant.updateBullet()
-		})
-		lane.zombiesArray.forEach(zombie => {
-			zombie.walk()
-			zombie.checkHit(lane.plantsArray)
-		})
-		if (lane.zombiesArray.some(zombie => zombie.health === 0))
-			lane.zombiesArray = lane.zombiesArray.filter(
-				zombie => zombie.health !== 0
-			)
-	})
-	requestAnimationFrame(selectedCeil)
+  map.forEach(lane => {
+    lane.plantsArray.forEach(plant => {
+      if (
+        lane.zombiesArray.length > 0 &&
+        lane.zombiesArray.some(zombie => {
+          const centerOfZombie =
+            zombie.htmlElement.getBoundingClientRect().x +
+            zombie.htmlElement.getBoundingClientRect().width / 3
+          const plantPosX = plant.htmlElement.getBoundingClientRect().x
+          return centerOfZombie > plantPosX
+        })
+      ) {
+        plant.shoot()
+      }
+      plant.updateBullet()
+    })
+    lane.zombiesArray.forEach(zombie => {
+      zombie.walk()
+      zombie.checkHit(lane.plantsArray)
+    })
+    if (lane.zombiesArray.some(zombie => zombie.health === 0))
+      lane.zombiesArray = lane.zombiesArray.filter(zombie => zombie.health !== 0)
+  })
+  requestAnimationFrame(selectedCeil)
 })
 
 let zombieSpawners = document.querySelectorAll('.zombie_spawner')
 
 zombieSpawners.forEach(spawn => {
-	spawn.addEventListener('click', e => {
-		let newElement = document.createElement('div')
-		newElement.classList.add('zombie')
+  spawn.addEventListener('click', e => {
+    let newElement = document.createElement('div')
+    newElement.classList.add('zombie')
 
     let healthBar = document.createElement('div')
     healthBar.classList.add('health_bar')
@@ -103,8 +103,6 @@ zombieSpawners.forEach(spawn => {
 
     spawn.appendChild(newElement)
 
-		map[parseInt(spawn.id)].zombiesArray.push(
-			new Zombie.RegularZombie(newElement)
-		)
-	})
+    map[parseInt(spawn.id)].zombiesArray.push(new Zombie.RegularZombie(newElement))
+  })
 })
